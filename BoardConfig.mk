8fceb0aa3b159bf2a9f9b8f6fa3dcd01cf9efd4d
hackpupg001-a11y <hackpupg001@gmail.com>
2026-03-31 06:18:43 +0700

    fix: print all build steps output

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,12 +124,10 @@ BOARD_SUPER_PARTITION_GROUPS := xiaomi_dynamic_partitions
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST := \
     system \
     system_ext \
-    system_dlkm \
     product \
     vendor \
     vendor_dlkm \
-    odm \
-    mi_ext
+    odm 
 
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 11270094848
 
