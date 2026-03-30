9abd82e061a1d7e215062d3c7d7f051aec3d0f39
dynamoz <hackpupg001@gmail.com>
2026-03-30 09:36:57 +0700

    Update twrp_myron.mk

--- twrp_myron.mk
+++ twrp_myron.mk
@@ -6,7 +6,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/xiaomi/myron/device.mk)
 
 # ─────────────────────────────────────────────────────────
