8f75f53be110cc854aad20e1e9c246c9ea5ca121
hackpupg001-a11y <hackpupg001@gmail.com>
2026-04-02 11:15:34 +0700

    fix: print all build steps output

--- device.mk
+++ device.mk
@@ -16,9 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # ─── API level ────────────────────────────────────────────────────────────────
-BOARD_SHIPPING_API_LEVEL   := 35
-PRODUCT_SHIPPING_API_LEVEL := 35
-PRODUCT_TARGET_VNDK_VERSION := 35
+BOARD_SHIPPING_API_LEVEL   := 34
+PRODUCT_SHIPPING_API_LEVEL := 34
+PRODUCT_TARGET_VNDK_VERSION := 34
 
 # ─── Dynamic partitions ───────────────────────────────────────────────────────
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
