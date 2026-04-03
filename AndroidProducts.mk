72aa18b5072fe2bae0a7da0385cafe2b8ba2cb11
dynamoz <hackpupg001@gmail.com>
2026-04-03 12:57:35 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,5 +7,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_myron.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_myron-eng \
-    twrp_myron-userdebug
+    twrp_myron-ap2a-eng \
+    twrp_myron-ap2a-userdebug
