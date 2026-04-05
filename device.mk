0d405fd83d1cd791066a1dce55e45bc81d41c878
hackpupg001-a11y <hackpupg001@gmail.com>
2026-04-06 00:31:12 +0700

    fix: print all build steps output

--- device.mk
+++ device.mk
@@ -60,7 +60,3 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/prebuilt/lib/firmware/cs40l26-dvl.bin:$(TARGET_COPY_OUT_RECOVERY)/root/lib/firmware/cs40l26-dvl.bin \
     $(DEVICE_PATH)/prebuilt/lib/firmware/cs40l26-svc.bin:$(TARGET_COPY_OUT_RECOVERY)/root/lib/firmware/cs40l26-svc.bin
 
-# Haptics AIDL libs — pulled from stock /system/lib64/
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/recovery/root/odm/lib64/android.hardware.vibrator-V1-ndk.so:$(TARGET_COPY_OUT_RECOVERY)/root/odm/lib64/android.hardware.vibrator-V1-ndk.so \
-    $(DEVICE_PATH)/recovery/root/odm/lib64/libxml2.so:$(TARGET_COPY_OUT_RECOVERY)/root/odm/lib64/libxml2.so
